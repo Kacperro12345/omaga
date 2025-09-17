bc519388338a976aa3a7ce765fe7fa8e1a1c3287
Kacperro12345 <kacperkosmider@interia.pl>
2025-09-17 17:11:45 +0200

    test edit

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import random
 
-x = random.randint(2)
-print(x)
+row = 0
+attempt = 0
+
+while row < 11:
+    x = random.randint(2)
+    if x == 2:
+        row += 1
+        attempt += 1
+    
+    if x != 2:
+        row = 0
+        attempt += 1
+
+    if row == 10:
+        print(f"udalo sie w {attempt} probach")
